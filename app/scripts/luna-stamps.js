e8f17b9a486b1de2eda6af71cf12b55aa47fb8c7
Tomaz <tomazkunst@gmail.com>
2016-03-30 15:18:49 +0200

    add var-s

--- app/scripts/luna-stamps.js
+++ app/scripts/luna-stamps.js
@@ -33,12 +33,12 @@ See GitHub project page for Documentation and License
           console.log("5")
           var log = Object()
           var touches = event.originalEvent.touches;
-          max_distance = 0
-          points = []
-          pivots = []
-          for (i = 0; i < 4; i++) {
-            for (j = i + 1; j < 5; j++) {
-              dis = vectorDistance(touches[i], touches[j])
+          var max_distance = 0
+          var points = []
+          var pivots = []
+          for (var i = 0; i < 4; i++) {
+            for (var j = i + 1; j < 5; j++) {
+              var dis = vectorDistance(touches[i], touches[j])
               if (dis > max_distance) {
                 max_distance = dis;
                 if (touches[i].pageY > touches[j].pageY) {
@@ -53,9 +53,9 @@ See GitHub project page for Documentation and License
           }
           log.max_distance = max_distance
           log.points = Array()
-          revertPivots = 0
+          var revertPivots = 0
             //check if under diagonal two points
-          for (i = 0; i < 5; i++) {
+          for (var i = 0; i < 5; i++) {
             if (pivots.indexOf(i) == -1) {
               if (nonAbsDistanceFromLine(touches[pivots[0]], touches[pivots[1]], touches[i]) < 0) {
                 revertPivots++;
@@ -68,7 +68,7 @@ See GitHub project page for Documentation and License
           }
           for (i = 0; i < 5; i++) {
             if (pivots.indexOf(i) == -1) {
-              data = Object()
+              var data = Object()
               data.distance = distanceFromLine(touches[pivots[0]], touches[pivots[1]], touches[i])
               data.height = pointInLineNearestToPoint(touches[pivots[0]], touches[pivots[1]], touches[i])
               log.points.push(data)
@@ -95,8 +95,8 @@ See GitHub project page for Documentation and License
       }
 
       function pointInLineNearestToPoint(a, b, c) {
-        B = vectorDistance(a, c)
-        A = distanceFromLine(a, b, c)
+        var B = vectorDistance(a, c)
+        var A = distanceFromLine(a, b, c)
         return Math.sqrt(B * B - A * A)
       }
 
@@ -121,8 +121,8 @@ See GitHub project page for Documentation and License
         console.log("get stamp")
 
         for (var i = 0; i < stamps.length; i++) {
-          norm = stamp.max_distance / stamps[i].distance
-          text = text + "max_distance: " + stamp.max_distance + "<br>"
+          var norm = stamp.max_distance / stamps[i].distance
+          var text = text + "max_distance: " + stamp.max_distance + "<br>"
           var calcDistance = Array()
           calcDistance[0] = Math.floor(Math.abs(stamp.points[0].distance / norm - stamps[i].distance1))
           calcDistance[1] = Math.floor(Math.abs(stamp.points[1].distance / norm - stamps[i].distance2))
